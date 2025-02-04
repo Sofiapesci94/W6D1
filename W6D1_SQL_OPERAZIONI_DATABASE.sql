d8ca0483ff0c251773fa532de44b6570e28f02a6
Sofiapesci94 <sofiapesci94@gmail.com>
2025-02-04 18:21:14 +0100

    Update W6D1_SQL_OPERAZIONI_DATABASE.sql

--- W6D1_SQL_OPERAZIONI_DATABASE.sql
+++ W6D1_SQL_OPERAZIONI_DATABASE.sql
@@ -5,12 +5,12 @@ SELECT  ProductKey, ProductAlternateKey, EnglishProductName, Color, StandardCost
 SELECT  ProductKey, ProductAlternateKey, EnglishProductName, Color, StandardCost, FinishedGoodsFlag AS WorkTable FROM dimproduct; -- mi sembra non abbia bisogno di un alias
 SELECT  ProductKey, ProductAlternateKey, EnglishProductName, Color, StandardCost, FinishedGoodsFlag FROM dimproduct WHERE FinishedGoodsFlag = 1;
 SELECT  ProductKey, ModelName, EnglishProductName, StandardCost, ListPrice, ProductAlternateKey FROM dimproduct WHERE ProductAlternateKey LIKE 'FR%' OR 'BK%';
-SELECT  ProductKey, ModelName, EnglishProductName, StandardCost, ListPrice, ProductAlternateKey FROM dimproduct WHERE ProductAlternateKey LIKE 'BK%' OR 'FR%';
+SELECT  ProductKey, ModelName, EnglishProductName, StandardCost, ListPrice, ProductAlternateKey FROM dimproduct WHERE ProductAlternateKey LIKE 'BK%' OR ProductAlternateKey LIKE 'FR%';
 SELECT  ProductKey, ModelName, EnglishProductName, StandardCost, ListPrice, ProductAlternateKey, Listprice - StandardCost AS Markup FROM dimproduct;
 SELECT ModelName as Productfinished, Listprice FROM dimproduct WHERE Listprice BETWEEN 1000 AND 2000 ORDER BY Listprice ASC;
 SELECT*FROM dimemployee;
 SELECT EmployeeKey, FirstName, LastName FROM dimemployee WHERE SalespersonFlag = 1;
 SELECT*FROM factresellersales;
-SELECT SalesOrderNumber, ProductKey, Orderdate, SalesAmount, SalesAmount - TotalProductCost AS Profit FROM factresellersales WHERE ProductKey IN (597,598,477,214);
+SELECT SalesOrderNumber, ProductKey, Orderdate, SalesAmount, SalesAmount - TotalProductCost AS Profit FROM factresellersales WHERE ProductKey IN (597,598,477,214) AND Orderdate > '2020-01-01';
 
 
